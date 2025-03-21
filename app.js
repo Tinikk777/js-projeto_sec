75450b139f8e9fe7391edcbb22908e9e7098a3ff
Nicolas Davi <nikdev.nk@gmail.com>
2025-03-20 21:07:32 -0300

    primeira_logica

---
+++ app.js
@@ -0,0 +1,16 @@
+// let titulo = document.querySelector('h1'); //Busca no HTML uma referência que contenha a tag H1;
+// titulo.innerHTML = 'Game Do Gatobô'; //Insira no HTML;
+// let subtitulo = document.querySelector('p'); //'querySelector' pega através da tag;
+// subtitulo.innerHTML = 'Adivinhe qual é o número entre 1 e 10...'; 
+
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag); //Busca no HTML uma referência que contenha a tag H1;
+    campo.innerHTML = texto; //Insira no HTML;
+}
+exibirTexto('h1','Jogo de Adivinhação')
+exibirTexto('p','Escolha um número entre 1 e 10...')
+
+function verificarChute(){
+    console.log('Apertou o botãozinho né?!')
+    
+}
